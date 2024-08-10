06ebc31e336a821ab6526776136ff5b59ca23f89
Daniel <daniel.torres@owasp.org>
2024-08-10 11:29:10 -0600

    update

--- webTester.sh
+++ webTester.sh
@@ -203,7 +203,7 @@ EOL
 )
 
 TOKEN_WPSCAN=${API_WPSCAN[$RANDOM % ${#API_WPSCAN[@]}]}
-echo "Version: 1.0 09082024"
+echo "Version: 1.0 10082024"
 echo "TOKEN_WPSCAN: $TOKEN_WPSCAN"
 
 if [[  ${SPEED} == "1" ]]; then
